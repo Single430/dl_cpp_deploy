9cce0b26d4f542690fb2595706a8134daea03842
super.single430 <Single430@users.noreply.github.com>
2020-08-18 09:42:19 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@
 rm -r build
 mkdir build
 cd build
-cmake -DCMAKE_PREFIX_PATH=/run/media/zbl/works/c.c++/src/libtorch  ..
+cmake -DCMAKE_PREFIX_PATH=/绝对路径/src/libtorch  ..
 cmake --build . --config Release --target demo -j 1
-#./demo /run/media/zbl/works/c.c++/crnn_libtorch/src/crnn.pt /run/media/zbl/works/c.c++/crnn_libtorch/src/keys.txt /run/media/zbl/works/c.c++/crnn_libtorch/src/test.jpg
 ./demo ../mnist/model/mnist.pt ../mnist/model/keys.txt ../mnist/model/test.png
